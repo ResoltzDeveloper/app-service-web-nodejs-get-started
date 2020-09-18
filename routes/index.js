e92d3edb97ad6d87fa65c71f2b2f96d3c3eeda5b
eobaah <eobaah@gmail.com>
2020-09-18 15:08:12 -0700

    test change

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test', function(req, res, next) {
+  res.send('this is a test');
+});
+
 module.exports = router;
